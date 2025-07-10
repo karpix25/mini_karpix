44616026adeeade7138800f733ee8a1acccd4464
karpix25 <nadaraya@gmail.com>
2025-07-10 14:35:35 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import './App.css';
 import Leaderboard from './Leaderboard';
 import Content from './Content';
+import CourseOverview from './CourseOverview'; // Добавить
+import LessonReader from './LessonReader'; // Добавить  
 import ArticleReader from './ArticleReader';
 import Profile from './Profile';
 
@@ -15,11 +17,18 @@ function App() {
             <Route path="/" element={<Profile />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/content" element={<Content />} />
+            
+            {/* Новые роуты для Skool-структуры */}
+            <Route path="/course/:courseId" element={<CourseOverview />} />
+            <Route path="/course/:courseId/lesson/:lessonId" element={<LessonReader />} />
+            
+            {/* Старый роут для совместимости */}
             <Route path="/article/:articleId" element={<ArticleReader />} />
           </Routes>
         </div>
         
         <Routes>
+            <Route path="/course/*" element={null} />
             <Route path="/article/:articleId" element={null} />
             <Route path="*" element={
                 <div className="nav-tabs">
