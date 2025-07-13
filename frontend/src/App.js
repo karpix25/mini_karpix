4d58f26c0e70c4135b39bde383b13485973c3ecd
karpix25git config --global user.email nadaraya@gmail.com <nadaraya@MacBook-Air-Nadaraya.local>
2025-07-13 14:53:07 -0300

    chore: смена пути для новой админки на /admin-panel для избежания конфликта с серверной

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import './App.css'; 
-import Profile from './Profile';
-import Content from './Content';
-import CourseOverview from './CourseOverview';
-import LessonReader from './LessonReader';
-import ArticleReader from './ArticleReader'; 
+import AdminPanel from './AdminPanel';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,38 +7,11 @@ function App() {
       <div className="App">
         <div className="content">
           <Routes>
-            {/* Главная страница, которая теперь включает Профиль и Лидерборд */}
-            <Route path="/" element={<Profile />} /> 
-            {/* Этот маршрут теперь можно удалить, так как он дублирует "/" */}
-            {/* <Route path="/leaderboard" element={<Profile />} /> */}
-            
-            <Route path="/content" element={<Content />} />
-            
-            <Route path="/course/:courseId" element={<CourseOverview />} />
-            <Route path="/course/:courseId/lesson/:lessonId" element={<LessonReader />} />
-            
-            <Route path="/article/:articleId" element={<ArticleReader />} />
+            <Route path="/admin" element={<AdminPanel />} />
           </Routes>
         </div>
-        
-        {/* Нижняя навигация */}
-        <div className="nav-tabs">
-            <NavLink to="/" className={({ isActive }) => `nav-tab ${isActive ? 'active' : ''}`}>
-                Профиль
-            </NavLink>
-            {/* УДАЛЯЕМ ЭТУ Вкладку Лидеры */}
-            {/* 
-            <NavLink to="/leaderboard" className={({ isActive }) => `nav-tab ${isActive ? 'active' : ''}`}>
-                Лидеры
-            </NavLink>
-            */}
-            <NavLink to="/content" className={({ isActive }) => `nav-tab ${isActive ? 'active' : ''}`}>
-                Контент
-            </NavLink>
-        </div>
+        {/* ... навигация ... */}
       </div>
     </Router>
   );
-}
-
-export default App;
+} 
